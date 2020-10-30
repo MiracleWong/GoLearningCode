03a683bf17607db812999a568f71f22a4e4b0a5f
MiracleWong <cfwr1991@126.com>
2020-10-30 16:31:17 +0800

    go template

---
+++ web/go_demo.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func tmpl(w http.ResponseWriter, r *http.Request) {
+	// 解析 html 文件，自动创建一个模板，关联到变量 t1 上
+	// 备注这个地方，写什么路径？绝对路径 or 相对路径
+	// web/test.html 时，在GoLand 中运行 OK，go run 则不行。反之
+	t1, err := template.ParseFiles("web/test.html")
+	if err != nil {
+		panic(err)
+	}
+	// 执行解析后的模板对象，合并&替换
+	t1.Execute(w, "Hello World")
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/tmpl", tmpl)
+	server.ListenAndServe()
+}
